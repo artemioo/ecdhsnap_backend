f0ddf336b15133de28845a272586beef87e4e112
artemioo <artemryb1@gmail.com>
2024-01-22 17:48:59 +0200

    change main func with depedencies

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,15 @@ import (
 	"log"
 
 	ecdhsnap "github.com/artemioo/ecdhsnap_backend"
+	"github.com/artemioo/ecdhsnap_backend/internal/database"
 	"github.com/artemioo/ecdhsnap_backend/internal/handler"
+	"github.com/artemioo/ecdhsnap_backend/internal/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	db := database.NewDatabase()
+	services := service.NewService(db)       // services depends on DB
+	handlers := handler.NewHandler(services) // handlers depends on services
 
 	srv := new(ecdhsnap.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
