62423f9a43d390a0d6adaeded63aec5f84e2edbe
artemioo <artemryb1@gmail.com>
2024-01-23 12:42:38 +0200

    add viper to project

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,25 @@ import (
 	"github.com/artemioo/ecdhsnap_backend/internal/database"
 	"github.com/artemioo/ecdhsnap_backend/internal/handler"
 	"github.com/artemioo/ecdhsnap_backend/internal/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	db := database.NewDatabase()
 	services := service.NewService(db)       // services depends on DB
 	handlers := handler.NewHandler(services) // handlers depends on services
 
 	srv := new(ecdhsnap.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs") // dir
+	viper.SetConfigName("config")  // filename
+	return viper.ReadInConfig()
 }
