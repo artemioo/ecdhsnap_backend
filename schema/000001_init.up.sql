bf8173d4fe491800444c72965b7c829c0bc0c39e
artemioo <artemryb1@gmail.com>
2024-01-29 17:36:47 +0200

    change user schema

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE users 
 (
     id serial not null unique,
-    username varchar(255) not null
+    username varchar(255) not null,
+    address varchar(255) not null,
+    pubkey varchar(255) not null
 );
