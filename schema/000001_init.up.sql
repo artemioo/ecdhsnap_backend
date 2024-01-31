b17ec8224d4e18c0cfcaadb6ae2bb341961f55be
artemioo <artemryb1@gmail.com>
2024-01-31 16:16:08 +0200

    add pair&message schemas

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -5,3 +5,18 @@ CREATE TABLE users
     address varchar(255) not null,
     pubkey varchar(255) not null
 );
+
+CREATE TABLE pair 
+(
+    id serial not null unique,
+    id_user_initiator int references users(id) not null,
+    id_user_partner int references users(id) not null
+);
+
+CREATE TABLE message 
+(
+    id serial not null unique,
+    id_pair int references pair(id),
+    encrypted_message varchar(255) not null,
+    sent_at timestamp
+);
