6387e700be5c11ed87b3d5f83e576033982f2ffd
artemioo <artemryb1@gmail.com>
2024-03-06 11:53:33 +0200

    update CreatePair method

--- internal/database/pair.go
+++ internal/database/pair.go
@@ -15,6 +15,15 @@ func NewPairPostgres(db *sqlx.DB) *PairPostgres {
 }
 
 func (r *PairPostgres) CreatePair(pair ecdhsnap.Pair) (int, error) {
+
+	// check if pair already exists
+	var existingPairID int
+	err := r.db.QueryRow("SELECT id FROM pair WHERE (id_user_initiator = $1 AND id_user_partner = $2) OR (id_user_initiator = $2 AND id_user_partner = $1)",
+		pair.Id_user_initiator, pair.Id_user_partner).Scan(&existingPairID)
+	if err == nil {
+		return existingPairID, nil
+	}
+
 	var id int
 	q := psql.Insert("pair").Columns("id_user_initiator", "id_user_partner").
 		Values(pair.Id_user_initiator, pair.Id_user_partner).
@@ -22,7 +31,7 @@ func (r *PairPostgres) CreatePair(pair ecdhsnap.Pair) (int, error) {
 		PlaceholderFormat(squirrel.Dollar).
 		RunWith(r.db)
 
-	err := q.QueryRow().Scan(&id)
+	err = q.QueryRow().Scan(&id)
 	if err != nil {
 		return 0, err
 	}
@@ -42,10 +51,13 @@ func (r *PairPostgres) GetRelatedPairs(userID int) ([]ecdhsnap.User, error) {
 	rows, err := r.db.Query(query, args...)
 	for rows.Next() {
 		var user ecdhsnap.User
-		if err := rows.Scan(&user.Id, &user.Name, &user.Address, &user.PubKey); err != nil {
+		if err := rows.Scan(&user.Id, &user.Username, &user.Address, &user.PubKey); err != nil {
 			return users, err
 		}
-		users = append(users, user)
+		if user.Id != userID {
+			users = append(users, user)
+		}
+
 	}
 	return users, err
 }
--- internal/handler/pair.go
+++ internal/handler/pair.go
@@ -3,19 +3,32 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
 
 	ecdhsnap "github.com/artemioo/ecdhsnap_backend"
-	"github.com/go-chi/chi/v5"
 )
 
+type SessionRequest struct {
+	SessionID string `json:"session_id"`
+}
+
 func (h *Handler) CreatePair(w http.ResponseWriter, r *http.Request) {
+	session, err_s := store.Get(r, "session-id")
+	if err_s != nil {
+		http.Error(w, "Internal Server Error: "+err_s.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	UserInitiatorId := session.Values["UserId"].(int)
+
 	var new_pair ecdhsnap.Pair
 	err := json.NewDecoder(r.Body).Decode(&new_pair)
 	if err != nil {
 		http.Error(w, "Invalid JSON data", http.StatusBadRequest)
 		return
 	}
+
+	new_pair.Id_user_initiator = UserInitiatorId
+
 	id, err := h.services.CreatePair(new_pair)
 	if err != nil {
 		http.Error(w, "Internal Server Error: "+err.Error(), http.StatusInternalServerError)
@@ -31,12 +44,12 @@ func (h *Handler) CreatePair(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) GetRelatedPairs(w http.ResponseWriter, r *http.Request) {
-	UserID := chi.URLParam(r, "userId")
-	UserIDInt, err := strconv.Atoi(UserID) // convert to int
-	id, err := h.services.GetRelatedPairs(UserIDInt)
+	session, err := store.Get(r, "session-id")
+	UserId := session.Values["UserId"].(int)
+	pairs, err := h.services.GetRelatedPairs(UserId)
 	if err != nil {
 		http.Error(w, "Internal Server Error: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Write([]byte(id))
+	w.Write([]byte(pairs))
 }
